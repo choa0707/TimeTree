bd3123f3b803d791fb0c39690727be92f1453e7a
choa0707 <choa0707@naver.com>
2020-08-27 15:04:35 +0900

    Navi Fragment

---
+++ app/src/main/java/com/example/timetree/FragmentCalender.java
@@ -0,0 +1,4 @@
+package com.example.timetree;
+
+public class FragmentCalender {
+}
---
+++ app/src/main/java/com/example/timetree/FragmentMemo.java
@@ -0,0 +1,24 @@
+package com.example.timetree;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentFeed extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        return view;
+    }
+}
