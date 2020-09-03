1d3beb07890a38d416b4f1ea5e8263a98d4ceb39
user <unjin95@naver.ocm>
2020-09-03 12:44:27 +0900

    sdwwfw

--- app/src/main/java/com/example/timetree/MainActivity.java
+++ app/src/main/java/com/example/timetree/MainActivity.java
@@ -2,14 +2,30 @@ package com.example.timetree;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.Button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(getApplicationContext(), SecondView.class);
+                Toast.makeText(getApplicationContext(),"버튼을 클릭했습니다.",Toast.LENGTH_LONG).show();
+                startActivity(intent2);
+            }
+        });
     }
 }
