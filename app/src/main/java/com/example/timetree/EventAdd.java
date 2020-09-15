ab82a2069f1a8a7d24b1f0b6235d27b1e40f41a9
user <unjin95@naver.ocm>
2020-09-15 21:14:23 +0900

    sdwwfw

---
+++ app/src/main/java/com/example/timetree/EventAdd.java
@@ -0,0 +1,14 @@
+package com.example.timetree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_add);
+    }
+}
--- app/src/main/java/com/example/timetree/GridViewListitem.java
+++ app/src/main/java/com/example/timetree/GridViewListitem.java
@@ -1,28 +1,26 @@
 package com.example.timetree;
 
-public class GridViewListitem {
-
+public class GridViewListItem {
     String title;
     String date;
 
-    public GridViewListitem(String title, String date) {
+    public GridViewListItem(String title, String date) {
         this.title = title;
         this.date = date;
     }
-
-    public String getTitle() { //Title 가져오는 메소드
+    public String getTitle() {
         return title;
     }
 
-    public String getDate() { //date 가져오는 메소드
-        return date;
-    }
-
-    public void setTitle(String title) { //Title 설정하는 메소드
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setDate(String date) { //Date 설정해주는 메소드
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
         this.date = date;
     }
 }
